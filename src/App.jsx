3e564a3779071cc2d632933cc9f3090925d24395
eliasjfscosta <christophe.fs.costa@gmail.com>
2023-09-25 21:00:54 +0200

    Inserting faq section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Download from './components/Download';
+import Faq from './components/Faq';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Download />
+      <Faq />
       <Newsletter />
       <Footer />
     </>
