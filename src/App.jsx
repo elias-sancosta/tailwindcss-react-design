fd4b1050f9f930690ce0d96fa08c2b8ca70df334
eliasjfscosta <christophe.fs.costa@gmail.com>
2023-09-25 20:20:06 +0200

    Inserting download, newsletter and footer sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Download from './components/Download';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Download />
+      <Newsletter />
+      <Footer />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="container relative mx-auto p-6">
